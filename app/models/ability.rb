566f59298e798737f919dbd175bda8bbb45bae98
Marcus Rocha <sif.marcusvinicius@gmail.com>
2017-12-15 18:25:21 +0000

    setting Ability for cancancan

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,13 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    if user
+      can :read, :all
+      can :mange, Post, user_id: user.id
+      can :mange, Follow, user_id: user.id
+      can :mange, Like, user_id: user.id
+      can :mange, Comment, user_id: user.id
+    end
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
